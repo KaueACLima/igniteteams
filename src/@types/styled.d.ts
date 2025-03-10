8a7cb511fc8cfd71b3c22543b40321a77ae190fa
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-03-09 23:17:25 -0300

    feat: created type styled

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components/native';
+import  type theme from '../theme';
+
+declare module 'styled-components' {
+    type ThemeType = typeof theme;
+
+    export interface DefatultTheme extends ThemeType {}
+}
