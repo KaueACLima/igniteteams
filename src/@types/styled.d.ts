7709c6d5a2ebba911222633252ee83c11a7bec69
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-03-12 23:26:11 -0300

    fix: correct DefaultTheme name in styled.d.ts

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,8 @@
 import 'styled-components/native';
-import  type theme from '../theme';
+import type theme from '../theme';
 
 declare module 'styled-components' {
     type ThemeType = typeof theme;
 
-    export interface DefatultTheme extends ThemeType {}
+    export interface DefaultTheme extends ThemeType {}
 }
