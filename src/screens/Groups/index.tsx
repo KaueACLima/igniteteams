880cf80df265031814e4884d61af21cf49032413
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-03-16 17:44:34 -0300

    refactor: update Groups component with HighLine and GroupCards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Rocket']);
+
   return (
     <Container>
       <Header />
+
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
+
+      <GroupCard title='Galera do ignite' />
     </Container>
   );
 }
